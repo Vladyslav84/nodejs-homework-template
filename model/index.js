c2637adc9370187cc6b970393d0406f070d87c07
Vladyslav84 <fakingbrutal@gmail.com>
2021-09-17 23:26:23 +0300

    1 комміт.

--- model/index.js
+++ model/index.js
@@ -1,15 +1,26 @@
-// const fs = require('fs/promises')
-// const contacts = require('./contacts.json')
 
-const listContacts = async () => {}
+const fs = require('fs/promises');
+const path = require('path');
 
-const getContactById = async (contactId) => {}
+const getContacts = async () => {
+  return JSON.parse(await fs.readFile(path.join(__dirname, './contacts.json'), 'utf8'));
+};
 
-const removeContact = async (contactId) => {}
+const listContacts = async () => {
+  return await getContacts();
+};
 
-const addContact = async (body) => {}
+const getContactById = async (contactId) => {
+  // console.log(contactId.id);
+  const data = await getContacts()
+  return await data.filter(contact => contact.id === contactId);
+};
 
-const updateContact = async (contactId, body) => {}
+const removeContact = async (contactId) => { }
+
+const addContact = async (body) => { }
+
+const updateContact = async (contactId, body) => { }
 
 module.exports = {
   listContacts,
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,12 +1,26 @@
 const express = require('express')
 const router = express.Router()
+const contacts = require('../../model/index');
 
 router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  try {
+    const data = await contacts.listContacts();
+    res.json({ status: 'succcess', code: 200, payload: { data } })
+  } catch (error) {
+    next(error)
+  }
 })
 
 router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  try {
+    const data = await contacts.getContactById(req.params.contactId);
+    if (data) {
+      return res.json({ status: 'succcess', code: 200, payload: { data } });
+    };
+
+  } catch (error) {
+    next(error)
+  }
 })
 
 router.post('/', async (req, res, next) => {
